a8173eee6289087641a4bda0a9b44a2f548a369b
karachevtsev <karachevtsev@yahoo.com>
2026-09-20 14:07:40 +0300

    Added vue-app example project

---
+++ lectures/examples/vue/02-vue/vue-app/src/router/index.ts
@@ -0,0 +1,39 @@
+import {createRouter, createWebHistory} from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+      /* Статичний import, HomeView.vue імпортується одразу під час завантаження JavaScript-модуля роутера.
+      1 - Завантаження app.js
+      2 - завантажується HomeView.vue
+      3 - створюється router
+      4 - користувач переходить на /
+      Тобто HomeView потрапляє в основний JS bundle (якщо bundler не вирішить інакше).
+       */
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue'),
+      /*
+      AboutView.vue не завантажується одразу. Спочатку Vue Router отримує функцію:
+      А коли користувач переходить /about:
+      1 - Router викликає import()
+      2 - завантажується About.[hash].js
+      3 - AboutView монтується
+      Це називається lazy loading / route-level code splitting.
+      Приклад коли є 20 сторінок.
+       */
+    },
+  ],
+})
+
+export default router
